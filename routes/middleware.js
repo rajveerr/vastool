5d48950c20d84b8ef38d3bf921c04dd635083ae5
Rajveer Rathore <rajveerr@gmail.com>
2017-03-13 10:16:13 -0400

    Implementing header design to default template

--- routes/middleware.js
+++ routes/middleware.js
@@ -17,11 +17,18 @@ var _ = require('lodash');
 	the navigation in the header, you may wish to change this array
 	or replace it with your own templates / logic.
 */
-exports.initLocals = function (req, res, next) {
+exports.initLocals = function(req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
-		{ label: 'Benefits', key: 'benefits', href: '/products' }
+		// { label: 'Home', key: 'home', href: '/' },
+		{
+			label: 'Contact',
+			key: 'contact',
+			href: '/contact'
+		}, {
+			label: 'Benefits',
+			key: 'benefits',
+			href: '/products'
+		}
 	];
 	res.locals.user = req.user;
 	next();
@@ -31,14 +38,16 @@ exports.initLocals = function (req, res, next) {
 /**
 	Fetches and clears the flashMessages before a view is rendered
 */
-exports.flashMessages = function (req, res, next) {
+exports.flashMessages = function(req, res, next) {
 	var flashMessages = {
 		info: req.flash('info'),
 		success: req.flash('success'),
 		warning: req.flash('warning'),
 		error: req.flash('error'),
 	};
-	res.locals.messages = _.some(flashMessages, function (msgs) { return msgs.length; }) ? flashMessages : false;
+	res.locals.messages = _.some(flashMessages, function(msgs) {
+		return msgs.length;
+	}) ? flashMessages : false;
 	next();
 };
 
@@ -46,7 +55,7 @@ exports.flashMessages = function (req, res, next) {
 /**
 	Prevents people from accessing protected pages when they're not signed in
  */
-exports.requireUser = function (req, res, next) {
+exports.requireUser = function(req, res, next) {
 	if (!req.user) {
 		req.flash('error', 'Please sign in to access this page.');
 		res.redirect('/keystone/signin');
