df9d4d8fad9ef66431deebdadc6249e7695d57d1
David Montoya <dmontoya@thoughtworks.com>
2017-03-14 16:32:24 -0400

    whitelist agent ids

--- routes/views/agent-login.js
+++ routes/views/agent-login.js
@@ -1,12 +1,34 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+					 _ = require('underscore');
 
 exports = module.exports = function (req, res) {
 
+	var WHITELISTED_AGENTS = [{
+		id: '5030167',
+		firstName: 'Shirley'
+	}, {
+		id: '7403455',
+		firstName: 'Rajveer'
+	}, {
+		id: '3664093',
+		firstName: 'Babs'
+	}, {
+		id: '9540002',
+		firstName: 'Alex'
+	}, {
+		id: '3134178',
+		firstName: 'Stefania'
+	}];
+
 	var view = new keystone.View(req, res);
 
+	function findAgentBy(id) {
+		return _.findWhere(WHITELISTED_AGENTS, { "id": id });
+	}
+
   view.on('init', function(next) {
-    var agentId = req.query.id;
-    if (agentId == 42) {
+		var agent = findAgentBy(req.query.id);
+    if (agent) {
 			next();
     } else {
 			res.status(401);
