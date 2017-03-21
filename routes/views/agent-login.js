7e15b6c5cadf6bebeb1b6e11405b879a7ad69caf
Rajveer Rathore <rajveerr@gmail.com>
2017-03-21 12:25:32 -0400

    Wiring a employer based journey from the login <Rajveer>

--- routes/views/agent-login.js
+++ routes/views/agent-login.js
@@ -1,7 +1,7 @@
 var keystone = require('keystone'),
-					 _ = require('underscore');
+	_ = require('underscore');
 
-exports = module.exports = function (req, res) {
+exports = module.exports = function(req, res) {
 
 	var WHITELISTED_AGENTS = [{
 		id: 'PDC503',
@@ -20,33 +20,78 @@ exports = module.exports = function (req, res) {
 		firstName: 'Stefania'
 	}];
 
+	var WHITELISTED_EMPLOYERS = [{
+		id: 'EMP503',
+		firstName: 'Employer by Shirley'
+	}, {
+		id: 'EMP740',
+		firstName: 'Employer by Rajveer'
+	}, {
+		id: 'EMP366',
+		firstName: 'Employer by Babs'
+	}, {
+		id: 'EMP954',
+		firstName: 'Employer by Alex'
+	}, {
+		id: 'EMP313',
+		firstName: 'Employer by Stefania'
+	}];
+
 	var view = new keystone.View(req, res);
 	var User = keystone.list(keystone.get('user model'));
 
 	function findAgentBy(id) {
-		return _.findWhere(WHITELISTED_AGENTS, { "id": id });
+		return _.findWhere(WHITELISTED_AGENTS, {
+			"id": id
+		});
 	}
 
-	function signIn(agent, success) {
+	function findEmployerBy(id) {
+		return _.findWhere(WHITELISTED_EMPLOYERS, {
+			"id": id
+		});
+	}
+
+	function signInAgent(agent, success) {
 		var user = new User.model({
-			name: {first: agent.firstName, last: agent.firstName},
+			name: {
+				first: agent.firstName,
+				last: agent.firstName
+			},
 			canAccessKeystone: false
 		});
 		keystone.session.signinWithUser(user, req, res, success);
 	}
 
-  view.on('post', function(next) {
+	function signInEmployer(employer, success) {
+		var user = new User.model({
+			name: {
+				first: employer.firstName,
+				last: employer.firstName
+			},
+			canAccessKeystone: false
+		});
+		keystone.session.signinWithUser(user, req, res, success);
+	}
+
+	view.on('post', function(next) {
 		var agent = findAgentBy(req.body.id);
-    if (agent) {
-			signIn(agent, function (user) {
+		var employer = findEmployerBy(req.body.id);
+		if (agent) {
+			signInAgent(agent, function(user) {
 				req.session.user = res.locals.user = user;
 				res.render('agent-home');
 			});
-    } else {
+		} else if (employer) {
+			signInEmployer(employer, function(user) {
+				req.session.user = res.locals.user = user;
+				res.render('employer-home');
+			});
+		} else {
 			res.status(401);
-      res.render('errors/401');
-    }
-  });
+			res.render('errors/401');
+		}
+	});
 
 	view.render('agent-login');
 };
