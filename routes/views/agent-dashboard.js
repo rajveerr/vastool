9028896818cfb6bee01381c905b9678d835c8340
Rajveer Rathore <rajveerr@gmail.com>
2017-03-21 11:33:29 -0400

    Importing VAS product benefits on agent dashbaord

--- routes/views/agent-dashboard.js
+++ routes/views/agent-dashboard.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone');
 var User = keystone.list(keystone.get('user model'));
 var Employer = keystone.list('Employer');
+var Product = keystone.list('Product');
 
 exports = module.exports = function(req, res) {
 
@@ -13,6 +14,15 @@ exports = module.exports = function(req, res) {
 
 	view.on('init', function(next) {
 		next();
+	});
+
+	view.on('get', function(next) {
+		Product.model.find({
+
+		}).exec(function(err, products) {
+			locals.data.products = products;
+			next(err);
+		});
 	});
 
 	view.on('post', function(next) {
