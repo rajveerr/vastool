d3af19180aedd91f9323f34b495f68a92d79c72b
Rajveer Rathore <rajveerr@gmail.com>
2017-03-23 15:49:23 -0400

    Introducing application-status page for agent dash board modal

---
+++ routes/views/application-status.js
@@ -0,0 +1,15 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	locals.data = {};
+
+	view.on('init', function(next) {
+		next();
+	});
+
+	view.render('application-status');
+};
