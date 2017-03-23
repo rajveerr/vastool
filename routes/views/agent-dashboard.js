126f7c02c4aa667575cd3b770a260d9525315779
Rajveer/Montoya <dmontoya@thoughtworks.com>
2017-03-23 13:20:54 -0400

    display employers in dashboard

--- routes/views/agent-dashboard.js
+++ routes/views/agent-dashboard.js
@@ -5,10 +5,23 @@ exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
 	var Product = keystone.list('Product');
+	var Employer = keystone.list('Employer');
 	var ProducerTip = keystone.list('ProducerTip');
 
 	var locals = res.locals;
 	locals.data = {};
+
+	function fetchProducts() {
+		return Product.model
+			.find()
+			.exec();
+	}
+
+	function fetchEmployersFor(userId) {
+		return Employer.model
+			.find({ userId: userId })
+			.exec();
+	}
 
 	view.on('init', function(next) {
 		ProducerTip.model
@@ -23,10 +36,20 @@ exports = module.exports = function(req, res) {
 	});
 
 	view.on('get', function(next) {
-		Product.model.find().exec(function(err, products) {
-			locals.data.products = products;
-			next(err);
-		});
+		fetchProducts()
+			.then(function(products) {
+				locals.data.products = products;
+
+				return
+					fetchEmployersFor(req.session.user.userID)
+					.then(function(employers) {
+						locals.data.employers = employers;
+						next();
+					});
+			})
+			.catch(function(err) {
+				next(err);
+			});
 	});
 
 	view.render('agent-dashboard');
