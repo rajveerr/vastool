8934e293a6c89cd000cc864f56b6ae85d873d715
Rajveer Rathore <rajveerr@gmail.com>
2017-03-24 13:12:00 -0400

    Minor usability improvement + adding css tooltip for abbriviations

--- routes/views/application-status.js
+++ routes/views/application-status.js
@@ -3,6 +3,8 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
+	var Employer = keystone.list('Employer');
+
 	var locals = res.locals;
 
 	locals.data = {};
