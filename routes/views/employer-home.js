d52e469eecd23788c0718b759fb6326f1d3b6c2c
Rajveer Rathore <rajveerr@gmail.com>
2017-04-05 10:28:13 -0400

    Updating Employer Journey <Rajveer>

--- routes/views/employer-home.js
+++ routes/views/employer-home.js
@@ -5,10 +5,18 @@ var PreferredBenefits = require('../../models/domain/PreferredBenefits');
 exports = module.exports = function(req, res) {
 
 	var view = new keystone.View(req, res);
+	var Product = keystone.list('Product');
 	var locals = res.locals;
 	var Employer = keystone.list('Employer');
 
+	var locals = res.locals;
 	locals.data = {};
+
+	function fetchProducts() {
+		return Product.model
+			.find()
+			.exec();
+	}
 
 	view.on('init', function(next) {
 		Employer.model
@@ -21,6 +29,21 @@ exports = module.exports = function(req, res) {
 					locals.data.preferredBenefitsRanking = [];
 				}
 				next();
+			})
+			.catch(function(err) {
+				next(err);
+			});
+	});
+
+	view.on('get', function(next) {
+		fetchProducts()
+			.then(function(products) {
+				locals.data.products = products;
+				fetchEmployersFor(req.session.user.userID)
+					.then(function(employers) {
+						locals.data.employers = employers;
+						next();
+					});
 			})
 			.catch(function(err) {
 				next(err);
